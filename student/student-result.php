8d7e20ccc1edc3b6d4d4cff3a8d98e028d6e36ca
Raj Singh <rajsinh2181@gmail.com>
2020-11-28 10:25:52 +0530

    student test done

---
+++ student/student-result.php
@@ -0,0 +1,22 @@
+<?php
+
+// For $_SERVER, we need to start the session
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Take Test</title>
+
+    <?php
+    require_once "../connection.php";
+    ?>
+</head>    
+
+<body>
+<h1>0</h1>
+</body>
+</html>
--- student/student-test.php
+++ student/student-test.php
@@ -18,21 +18,39 @@ session_start();
 
 <?php
     require_once "../connection.php";
+    if(isset($_POST['submit'])) {
+    }
+     ?>
+  
+    <?php
     $testid = $_GET['id'];
     $q = "SELECT * FROM question WHERE test_id='$testid';";
-    echo $testid;
-    $query = mysqli_query($con, $q);
-    while($res = mysqli_fetch_assoc($query)) {​​
-    ?>
+    
+        $query = mysqli_query($con, $q);?>
+         <form action="student-result.php" method="post">
+        <?php
+            while($res = mysqli_fetch_array($query)) {​​
+            ?>
 
-    <h3><?php echo $res['question_no'];?></h3><br>
-    <h4><?php echo $res['choice_a'];?></h4><br>
-    <h4><?php echo $res['choice_b'];?></h4><br>
-    <h4><?php echo $res['choice_c'];?></h4><br>
-    <h4><?php echo $res['choice_d']; ?></h4>
-    <?php
-    }​​
-?>
+                <h3><?php echo $res['question_no'];?>.<?php echo $res['statement'];?></h3>
+
+                    <input type="radio" name="quizcheck[<?php echo $res['question_id'];?>" value="<?php echo $res['choice_a'] ?>;">
+                    <?php echo $res['choice_a']?>
+
+                    <input type="radio" name="quizcheck[<?php echo $res['question_id'];?>" value="<?php echo $res['choice_b']?>;">
+                    <?php echo $res['choice_b']?>
+
+                    <input type="radio" name="quizcheck[<?php echo $res['question_id'];?>" value="<?php echo $res['choice_c'] ?>;">
+                    <?php echo $res['choice_c']?>
+
+                    <input type="radio" name="quizcheck[<?php echo $res['question_id'];?>" value="<?php echo $res['choice_d'] ?>;">
+                    <?php echo $res['choice_d']?>
+
+            <?php
+            }​​
+            ?>
+            <button type="submit" name="submit" value="submit" class="btn-signin" >Submit</button>
+        </form>
 
 
 </body>
